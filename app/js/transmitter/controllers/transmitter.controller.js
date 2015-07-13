9397c17240c7add41fc983d3ccc9b3ea26baa7dd
Pablo Andres Dorado Suarez <pandres95@boolinc.co>
2015-07-13 04:32:46 -0500

    Generate call

--- app/js/transmitter/controllers/transmitter.controller.js
+++ app/js/transmitter/controllers/transmitter.controller.js
@@ -2,13 +2,40 @@
     'use strict';
 
     angular
-        .module('webrtc.transmitter')
-        .controller('TransmitterController', TransmitterController);
+    .module('webrtc.transmitter')
+    .controller('TransmitterController', TransmitterController);
 
-        function TransmitterController($log) {
-            var vm = this;
+    function TransmitterController($scope, $log, Media, PeerConnection) {
 
-            $log.info('Hello world');
-        }
+        var vm = this;
+        vm.connectedClients = [];
+        vm.transmitterData = {};
+
+        vm.load = function () {
+
+            Media.load().then(function (stream) {
+                vm.stream = stream;
+                return PeerConnection.connect('whoopsie');
+            }).then(function (peer) {
+                vm.bindEvents(peer);
+            }).catch(function (err) {
+                $log.error(err);
+            });
+        };
+
+        vm.bindEvents = function (peer) {
+
+            peer.on('connect', function (connection) {
+                vm.connectedClients.push({
+                    id: connection.id,
+                    connDate: new Date()
+                });
+                peer.call(connection.id, vm.stream);
+            });
+
+        };
+
+        vm.load();
+    }
 
 })();
